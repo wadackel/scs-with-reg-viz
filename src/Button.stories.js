68f0b6684d3195a2e53bd9b7d41b6f017b15ca87
Maciej Jaroszewski <maciej.jaroszewski@netguru.pl>
2018-11-19 11:07:24 +0100

    Initial setup

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withScreenshot } from 'storybook-chrome-screenshot';
-import { Button } from './Button';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withScreenshot } from "storybook-chrome-screenshot";
+import { Button } from "./Button";
 
-storiesOf('Button', module)
+storiesOf("Button", module)
   .addDecorator(withScreenshot())
-  .add('with default style', () => (
-    <Button>Default</Button>
-  ))
-  .add('with primary style', () => (
-    <Button primary>Primary</Button>
-  ));
+  .add("with default style", () => <Button primary>Default</Button>)
+  .add("with primary style", () => <Button primary>Primary</Button>);
